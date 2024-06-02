0ec9fd034eaaf9819bc6f9e0590c975080f5e410
jameson512 <2867557054@qq.com>
2024-06-02 15:28:08 +0800

    features:add .env set

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ torch._dynamo.config.cache_size_limit = 64
 torch._dynamo.config.suppress_errors = True
 torch.set_float32_matmul_precision('high')
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
-VERSION='0.6'
+VERSION='0.7'
 
 def get_executable_path():
     # 这个函数会返回可执行文件所在的目录
@@ -45,19 +45,21 @@ import hashlib,webbrowser
 from modelscope import snapshot_download
 import numpy as np
 import time
+
 # 读取 .env 变量
 WEB_ADDRESS = os.getenv('WEB_ADDRESS', '127.0.0.1:9966')
 
 # 默认从 modelscope 下载模型,如果想从huggingface下载模型，请将以下3行注释掉
 CHATTTS_DIR = snapshot_download('pzc163/chatTTS',cache_dir=MODEL_DIR)
 chat = ChatTTS.Chat()
-chat.load_models(source="local",local_path=CHATTTS_DIR)
+# 通过将 .env中 compile设为false，禁用推理优化. 其他为启用。一定情况下通过禁用，能提高GPU效率
+chat.load_models(source="local",local_path=CHATTTS_DIR, compile=True if os.getenv('compile','true').lower()!='false' else False)
 
 # 如果希望从 huggingface.co下载模型，将以下注释删掉。将上方3行内容注释掉
 #os.environ['HF_HUB_CACHE']=MODEL_DIR
 #os.environ['HF_ASSETS_CACHE']=MODEL_DIR
 #chat = ChatTTS.Chat()
-#chat.load_models()
+#chat.load_models(compile=True if os.getenv('compile','true').lower()!='false' else False)
 
 
 
@@ -140,7 +142,7 @@ def tts():
     md5_hash = hashlib.md5()
     md5_hash.update(f"{text}-{voice}-{skip_refine}-{prompt}".encode('utf-8'))
     datename=datetime.datetime.now().strftime('%Y%m%d-%H_%M_%S')
-    filename = datename+'-'+md5_hash.hexdigest() + ".wav"
+    filename = datename+'-'+md5_hash.hexdigest()[:8] + ".wav"
 
     start_time = time.time()
 
